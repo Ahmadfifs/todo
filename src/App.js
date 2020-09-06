5cced4581f7d2928abe1ebd820df4889c101400e
Ahmadfifs <a7-h-m@outlook.com>
2020-09-06 22:41:59 +0300

    start react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList'
 
 function App() {
+
+  const [input , setInput] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        Todo List
       </header>
+      <p>hello world this is Faisal Alfaify</p>
+      <Form setInput={setInput}/>
+      <TodoList/>
     </div>
   );
 }
